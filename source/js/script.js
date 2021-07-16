ed708690a6bee4e2ef92f32cc0d108621c89a949
nanawebdev <nanaprettysinger@gmail.com>
2021-07-16 12:59:12 +0300

    added mobile version

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,15 @@
 const openCardButton = document.querySelector('._open-card');
 const closeCardButton = document.querySelector('._close-card');
 const card = document.querySelector('.card');
+const overlay = document.querySelector('.overlay');
 
 openCardButton.addEventListener('click', () => {
   card.classList.add('showCard')
+  overlay.style.display = 'block'
 
   closeCardButton.addEventListener('click', () => {
     card.classList.remove('showCard')
+    overlay.style.display = 'none'
 
     openCardButton.removeEventListener('click', () => {
       card.classList.add('showCard')
